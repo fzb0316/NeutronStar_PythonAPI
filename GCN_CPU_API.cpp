7c5a2e980631f6d3439f647afe223f2979c2a8e9
fzb0316 <fzb0316@163.com>
2022-05-18 16:20:23 +0800

    重构

--- GCN_CPU_API.cpp
+++ GCN_CPU_API.cpp
@@ -4,27 +4,6 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(GCN_CPU, m) {
-    m.def("get_time", &get_time, "get the time of system");
-
-    py::class_<GraphOperation>(m, "Graph_Operation")
-        .def(py::init([](Graph<Empty> *graph_, VertexSubset* active)
-               {return new GraphOperation(graph_, active); }))
-               
-        .def("PropagateForwardCPU_Lockfree_multisockets", &GraphOperation::PropagateForwardCPU_Lockfree_multisockets, 
-            "gather neithbour's vertex feature, the intermediate value is stored in Y", 
-            py::arg("X"), py::arg("Y"), py::arg("subgraphs"));
-
-
-    py::class_<nts::autodiff::ComputionPath>(m, "ComputionPath")
-        .def(py::init([](GraphOperation *gt_, std::vector<CSC_segment_pinned *> subgraphs_)
-               {return new nts::autodiff::ComputionPath(gt_, subgraphs_); }))
-        
-        .def("op_push", &nts::autodiff::ComputionPath::op_push, 
-            "push the operation and intermediate result into ComputationPath, for backward propagation")
-        .def("self_backward", &nts::autodiff::ComputionPath::self_backward,
-            "do the backward propagation using the value that we stored while doing forward computation");
-    
-
     py::class_<GCN_CPU_impl>(m, "GCN_CPU")
         // .def(py::init<Graph<Empty> *, int>())
         .def(py::init([](Graph<Empty> *graph_, int iterations_, bool process_local = false, bool process_overlap = false)
@@ -36,7 +15,6 @@ PYBIND11_MODULE(GCN_CPU, m) {
         .def("_vertexForward", &GCN_CPU_impl::vertexForward, "   ", py::arg("a"), py::arg("x"))
 
         .def("_Loss", &GCN_CPU_impl::Loss, "init a loss function")
-        //.def("_backward",)//backward is a deeper class,don't think better up to now.
         .def("_update", &GCN_CPU_impl::Update, "update")
         .def("_run", &GCN_CPU_impl::run, "run the GCN_CPU")
         .def("_Forward", &GCN_CPU_impl::Forward, "forward")
